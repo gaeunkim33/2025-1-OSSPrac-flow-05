00e1e5c9e5ddb83f341c4fa512428dd28f00345f
ohhyeryeong <2023110466@dgu.ac.kr>
2025-05-03 23:43:23 +0900

    add major in Subject3_1

--- Subject3_1/ex4.py
+++ Subject3_1/ex4.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+@app.route('/')
+def input():
+    return render_template('input.html')
+
+@app.route('/result',methods=['POST','GET'])
+def result():
+    if request.method =='POST':
+       result=dict()
+       result['Major']=request.form.get('Major')
+       return render_template('result.html',result=result)
+
+if __name__ =='__main__':
+     app.run(debug=True)
 
