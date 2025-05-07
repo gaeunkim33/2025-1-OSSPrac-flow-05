bdc1840e9ad682a76ee21534790900e140043dd8
ybkim453 <yb1553@dgu.ac.kr>
2025-05-07 15:12:21 +0900

    Add language

--- Subject3_1/ex4.py
+++ Subject3_1/ex4.py
@@ -12,6 +12,7 @@ def result():
     # 각 학생의 이름과 학번 데이터를 리스트로 받음
     names = request.form.getlist('name[]')
     student_numbers = request.form.getlist('StudentNumber[]')
+    languages = request.form.getlist('language[]')
     
     genders = []
         
@@ -20,7 +21,7 @@ def result():
         genders.append(gender_value)
 
     # 데이터를 템플릿으로 전달하여 출력 페이지 생성
-    return render_template('result.html', students=zip(names, student_numbers, genders))
+    return render_template('result.html', students=zip(names, student_numbers, genders), languages=languages)
 
 if __name__ == '__main__':
     app.run(debug=True)
