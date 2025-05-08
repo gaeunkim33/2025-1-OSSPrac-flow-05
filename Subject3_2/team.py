24fc51aacbc824c77721d13f0c1377a088346756
GEKim <kge5815@dgu.ac.kr>
2025-05-08 21:35:04 +0900

    Add MBTI

--- Subject3_2/team.py
+++ Subject3_2/team.py
@@ -2,14 +2,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-# 최초 메인 페이지를 보여주는 루트 경로
 @app.route('/')
-def index():
-    return render_template('index.html')  # 위 HTML 코드를 form.html 파일로 저장해야 합니다
-
-# 학생 정보를 입력하는 경로
-
-@app.route('/input')
 def input():
     return render_template('input.html')
 
@@ -19,8 +12,8 @@ def result():
     # 각 학생의 이름과 학번 데이터를 리스트로 받음
     names = request.form.getlist('name[]')
     student_numbers = request.form.getlist('StudentNumber[]')
-    email = request.form.getlist('email[]')
     major = request.form.getlist('major[]')
+    mbti = request.form.getlist('mbti[]')
 
     genders = []
     languages = []
@@ -32,11 +25,8 @@ def result():
         languages.append(langs)
 
     # 데이터를 템플릿으로 전달하여 출력 페이지 생성
-    return render_template('result.html', students=zip(names, student_numbers, genders, email, major, languages))
+    return render_template('result.html', students=zip(names, student_numbers, genders, major, languages, mbti))
 
-@app.route('/contact')
-def contact_info():
-    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
